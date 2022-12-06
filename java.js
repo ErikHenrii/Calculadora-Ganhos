e86b9152a30eec949671637efd996f37ea193859
Erik Henrique da Silveira <93984930+ErikHenrii@users.noreply.github.com>
2022-12-06 14:53:27 -0300

    style

--- java.js
+++ java.js
@@ -3,13 +3,15 @@ function somar() {
     var tn2 = document.getElementById('txtn2')
     var tn3 = document.getElementById('txtn3')
     var tn4 = document.getElementById('txtn4')
+    var tn5 = document.getElementById('txtn5')
     var res = document.getElementById('res')
     var n1 = Number(tn1.value)
     var n2 = Number(tn2.value)
     var n3 = Number (tn3.value)
     var n4 = Number(tn4.value)
+    var n5 = Number(tn5.value)
     var s1 = n1 / n2 * n3
-    var s2 = n4 - s1
+    var s2 = n4 +n5 - s1
     res.innerHTML = (`Seu Ganho Liquido do dia é de $${s2.toFixed(2)} Reais`)
     res.style.background='green'
     
